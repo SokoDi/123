96b3ac79d465649235882760f4130c0f9fc436d2
Дмитрий <111419427+SokoDi@users.noreply.github.com>
2023-06-16 00:32:56 +0300

    Initial commit

---
+++ api_yamdb/api/mixins.py
@@ -0,0 +1,14 @@
+from api.permissions import IsAdminOrReadOnly
+from rest_framework import filters, mixins, viewsets
+
+
+class ListCreateDestroyGenericViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+    permission_classes = (IsAdminOrReadOnly,)
+    lookup_field = 'slug'
